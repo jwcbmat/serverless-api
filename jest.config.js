f97a18538891a99a790846eba196f5226ec46d3d
jwcbmat <johnathan.wcb@gmail.com>
2022-12-14 01:56:35 -0300

    config jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 const config = {
     verbose: true,
-}
-
-module.exports = config
+  
+    // Especifica quais arquivos de teste devem ser executados pelo Jest
+    testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  
+    // Ignora determinadas pastas ou arquivos durante a execução dos testes
+    testPathIgnorePatterns: ["/node_modules/", "__fixtures__"],
+  
+    // Especifica quais extensões de arquivo devem ser consideradas como módulos pelo Jest
+    moduleFileExtensions: ["ts", "js"],
+  };
+  
+  export default config;
+  
